3b251d584b4700a914a2d29f64717ade4c30691e
Andra <aw124@hdm-stuttgart.de>
2016-10-30 17:54:59 +0100

    NoteAdministrationAsync init methode erstellt

--- Application/src/com/hdm/Application/shared/NoteAdministrationAsync.java
+++ Application/src/com/hdm/Application/shared/NoteAdministrationAsync.java
@@ -1,9 +1,4 @@
-package com.hdm.Application.shared;
 
-import com.google.gwt.user.client.rpc.AsyncCallback;
 
-public interface NoteAdministrationAsync {
 
-	void init(AsyncCallback<Void> callback);
 
-}
