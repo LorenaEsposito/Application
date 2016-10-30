445843d1e73332a9d417d520a2763a7f033c2615
Andra <aw124@hdm-stuttgart.de>
2016-10-30 18:15:36 +0100

    KKommentare hinzugefügt BusinessObject, DueDate,Note, Notebook,Noteobject

--- Application/src/com/hdm/Application/shared/bo/DueDate.java
+++ Application/src/com/hdm/Application/shared/bo/DueDate.java
@@ -11,17 +11,30 @@ public class DueDate extends BusinessObject {
 	private static final long serialVersionUID = 1L;
 	private Date dueDate; 
 	private int nID;
+	/**
+	 * @return the dueDate
+	 */
 	public Date getDueDate() {
 		return dueDate;
 	}
+	/**
+	 * @param dueDate the dueDate to set
+	 */
 	public void setDueDate(Date dueDate) {
 		this.dueDate = dueDate;
 	}
+	/**
+	 * @return the nID
+	 */
 	public int getnID() {
 		return nID;
 	}
+	/**
+	 * @param nID the nID to set
+	 */
 	public void setnID(int nID) {
 		this.nID = nID;
-	} 
+	}
 
+	
 }
--- Application/src/com/hdm/Application/shared/bo/Note.java
+++ Application/src/com/hdm/Application/shared/bo/Note.java
@@ -8,56 +8,100 @@ public class Note extends NoteObject {
 	
 	
 	private int nID; 
+	/**
+	 * @return the nID
+	 */
+	public int getnID() {
+		return nID;
+	}
+	/**
+	 * @param nID the nID to set
+	 */
+	public void setnID(int nID) {
+		this.nID = nID;
+	}
+	/**
+	 * @return the nTitle
+	 */
+	public String getnTitle() {
+		return nTitle;
+	}
+	/**
+	 * @param nTitle the nTitle to set
+	 */
+	public void setnTitle(String nTitle) {
+		this.nTitle = nTitle;
+	}
+	/**
+	 * @return the nSubtitle
+	 */
+	public String getnSubtitle() {
+		return nSubtitle;
+	}
+	/**
+	 * @param nSubtitle the nSubtitle to set
+	 */
+	public void setnSubtitle(String nSubtitle) {
+		this.nSubtitle = nSubtitle;
+	}
+	/**
+	 * @return the nbID
+	 */
+	public int getNbID() {
+		return nbID;
+	}
+	/**
+	 * @param nbID the nbID to set
+	 */
+	public void setNbID(int nbID) {
+		this.nbID = nbID;
+	}
+	/**
+	 * @return the nContent
+	 */
+	public String getnContent() {
+		return nContent;
+	}
+	/**
+	 * @param nContent the nContent to set
+	 */
+	public void setnContent(String nContent) {
+		this.nContent = nContent;
+	}
+	/**
+	 * @return the modDate
+	 */
+	public Date getModDate() {
+		return modDate;
+	}
+	/**
+	 * @param modDate the modDate to set
+	 */
+	public void setModDate(Date modDate) {
+		this.modDate = modDate;
+	}
+	/**
+	 * @return the creDate
+	 */
+	public Date getCreDate() {
+		return creDate;
+	}
+	/**
+	 * @param creDate the creDate to set
+	 */
+	public void setCreDate(Date creDate) {
+		this.creDate = creDate;
+	}
 	private String nTitle; 
 	private String nSubtitle; 
 	private int nbID; 
 	private String nContent; 
 	private Date modDate; 
 	private Date creDate;
-	public int getnID() {
-		return nID;
-	}
-	public void setnID(int nID) {
-		this.nID = nID;
-	}
-	public String getnTitle() {
-		return nTitle;
-	}
-	public void setnTitle(String nTitle) {
-		this.nTitle = nTitle;
-	}
-	public String getnSubtitle() {
-		return nSubtitle;
-	}
-	public void setnSubtitle(String nSubtitle) {
-		this.nSubtitle = nSubtitle;
-	}
-	public int getNbID() {
-		return nbID;
-	}
-	public void setNbID(int nbID) {
-		this.nbID = nbID;
-	}
-	public String getnContent() {
-		return nContent;
-	}
-	public void setnContent(String nContent) {
-		this.nContent = nContent;
-	}
-	public Date getModDate() {
-		return modDate;
-	}
-	public void setModDate(Date modDate) {
-		this.modDate = modDate;
-	}
-	public Date getCreDate() {
-		return creDate;
-	}
-	public void setCreDate(Date creDate) {
-		this.creDate = creDate;
+
 	}
 	
 	
 
  
-}
+
