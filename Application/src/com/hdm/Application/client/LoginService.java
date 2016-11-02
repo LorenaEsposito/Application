cd9df300058f82e73123888bff2d7b4759066048
Lorena Esposito <le014@hdm-stuttgart.de>
2016-11-02 10:26:38 +0100

    LoginService erstellt

---
+++ Application/src/com/hdm/Application/client/LoginService.java
@@ -0,0 +1,18 @@
+package com.hdm.Application.client;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+import com.hdm.Application.shared.LoginInfo;
+
+/**
+ * Das synchrone Gegenstueck des Interface {@link LoginServiceAsync}. Es erfolgt
+ * hier keine weitere Dokumentation. Fuer weitere Informationen: siehe die
+ * Implementierung des Interface {@link LoginServiceImpl}
+ * 
+ * @author Lorena Esposito
+ */
+@RemoteServiceRelativePath("login")
+public interface LoginService extends RemoteService {
+	public LoginInfo login(String requestUri);
+}
