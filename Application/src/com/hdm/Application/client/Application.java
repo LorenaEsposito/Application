528c0a38a637698288e5422bee7d256b2e222eb8
Lorena Esposito <le014@hdm-stuttgart.de>
2016-11-08 18:11:48 +0100

    Mit Domi gemerged

--- Application/src/com/hdm/Application/client/Application.java
+++ Application/src/com/hdm/Application/client/Application.java
@@ -6,7 +6,6 @@ import com.hdm.Application.client.gui.LoginServiceAsync;
 import com.hdm.Application.client.gui.NoteOverviewView;
 import com.hdm.Application.client.gui.Update;
 import com.hdm.Application.shared.LoginInfo;
-
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -17,6 +16,7 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
@@ -30,17 +30,16 @@ public class Application implements EntryPoint {
   * returns an error.
   */
  
- 
- LoginInfo loginInfo = new LoginInfo();
-  private VerticalPanel loginPanel = new VerticalPanel();
-  private Label loginLabel = new Label("Bitte melde dich mit deinem Google Account an, um Notework nutzen zu k�nnen. Klicke auf Login und los geht's!");
-  private Anchor signInLink = new Anchor("Login");
-  public Anchor signOutLink = new Anchor("Logout");
-  public final FlowPanel detailPanel = new FlowPanel();
-  final VerticalPanel navigationPanel = new VerticalPanel();
-  LoginServiceAsync loginService = GWT.create(LoginService.class);
- private static final String SERVER_ERROR = "An error occurred while "
-   + "attempting to contact the server. Please check your network " + "connection and try again.";
+// LoginInfo loginInfo = new LoginInfo();
+//  private VerticalPanel loginPanel = new VerticalPanel();
+//  private Label loginLabel = new Label("Bitte melde dich mit deinem Google Account an, um Notework nutzen zu k�nnen. Klicke auf Login und los geht's!");
+//  private Anchor signInLink = new Anchor("Login");
+//  public Anchor signOutLink = new Anchor("Logout");
+//  public final FlowPanel detailPanel = new FlowPanel();
+//  final VerticalPanel navigationPanel = new VerticalPanel();
+//  LoginServiceAsync loginService = GWT.create(LoginService.class);
+// private static final String SERVER_ERROR = "An error occurred while "
+//   + "attempting to contact the server. Please check your network " + "connection and try again.";
 
  /**
   * Create a remote service proxy to talk to the server-side Greeting service.
@@ -50,60 +49,59 @@ public class Application implements EntryPoint {
  
  public void onModuleLoad() {
     
-   navigationPanel.setVisible(false); 
-    
-      // Check login status using login service.
-     LoginServiceAsync loginService = GWT.create(LoginService.class);
-     loginService.login("http://127.0.0.1:8888/Application.html", new AsyncCallback<LoginInfo>() {
-     public void onFailure(Throwable error) {
-
-      // DialogBox d = new DialogBox();
-      Window.alert("Fehler: Harter Fehler");
-       // d.show();
-      }
-
-      public void onSuccess(LoginInfo result) {
-        loginInfo = result;  
-      if (loginInfo.isLoggedIn()){ 
-       Window.alert("Erfolgreich eingeloggt");
-       loadGUI();
-        }
-     
-        else{ 
-        	loadLogin();
-            Window.alert("Einloggen fehlgeschlagen");
-           }
-           }
-           });
-          }
+//   navigationPanel.setVisible(false); 
+//    
+//      // Check login status using login service.
+//     LoginServiceAsync loginService = GWT.create(LoginService.class);
+//     loginService.login("http://127.0.0.1:8888/Application.html", new AsyncCallback<LoginInfo>() {
+//     public void onFailure(Throwable error) {
+//
+//      // DialogBox d = new DialogBox();
+//      Window.alert("Fehler: Harter Fehler");
+//       // d.show();
+//      }
+//
+//      public void onSuccess(LoginInfo result) {
+//        loginInfo = result;  
+//      if (loginInfo.isLoggedIn()){ 
+//       Window.alert("Erfolgreich eingeloggt");
+//       
+//        }
+//     
+//        else{ 
+//        	loadLogin();
+//            Window.alert("Einloggen fehlgeschlagen");
+//           }
+//           }
+//           });
+//          }
+// 
  
- 
- private void loadLogin() {
-     
-     Cookies.setCookie("usermail", null);
-     loginPanel.setStyleName("login");
-     signInLink.setHref(loginInfo.getLoginUrl());
-     
-     signInLink.setStyleName("loginLink");
-     loginLabel.setStyleName("loginLink2");
-     
-     
-     loginPanel.add(loginLabel);
-     loginPanel.add(signInLink);
-     
-     Cookies.setCookie("userMail", null);
-     Cookies.setCookie("userID", null);
-     RootPanel.get("Starter").add(loginPanel);     
-    }
- 
- public void loadGUI() {
+// private void loadLogin() {
+//     
+//     Cookies.setCookie("usermail", null);
+//     loginPanel.setStyleName("login");
+//     signInLink.setHref(loginInfo.getLoginUrl());
+//     
+//     signInLink.setStyleName("loginLink");
+//     loginLabel.setStyleName("loginLink2");
+//     
+//     
+//     loginPanel.add(loginLabel);
+//     loginPanel.add(signInLink);
+//     
+//     Cookies.setCookie("userMail", null);
+//     Cookies.setCookie("userID", null);
+//     RootPanel.get("Starter").add(loginPanel);     
+//    }
+// 
+// public void loadGUI() {
 		VerticalPanel navPanel = new VerticalPanel();
-
+		
 	    /*
 	     * Das VerticalPanel wird einem DIV-Element namens "Navigator" in der
 	     * zugehörigen HTML-Datei zugewiesen und erhält so seinen Darstellungsort.
 	     */
-	    RootPanel.get("Navigator").add(navPanel);
 	    
 	    /*
 	     * Erstellung des ersten Navibar-Buttons zum hinzufügen neuer Notizen
@@ -115,6 +113,7 @@ public class Application implements EntryPoint {
 	    
 	    navPanel.add(createNoteButton);
 	    
+	    RootPanel.get("Navigator").add(navPanel);
 	    createNoteButton.addClickHandler(new ClickHandler() {
 	  	public void onClick(ClickEvent event) {
 	          /*
@@ -126,7 +125,7 @@ public class Application implements EntryPoint {
 	          RootPanel.get("Details").add(update);
 	    }
 	    });
-	    final Button notebookButton = new Button("+");
+	    final Button notebookButton = new Button("My Recipes");
 	    
 	    notebookButton.setStyleName("notework-menubutton");
 	    
