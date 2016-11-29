95d6039f0a9c75cc5c1419dd76a626930f596f0e
MariusKlepser <mk212@hdm-stuttgart.de>
2016-11-29 14:09:39 +0100

    Fehler in der DBConnection.java behoben.

--- Application/src/com/hdm/Application/server/db/DBConnection.java
+++ Application/src/com/hdm/Application/server/db/DBConnection.java
@@ -22,7 +22,7 @@ public class DBConnection {
      * Die URL fuer die Google Cloud-SQL Datenbank - angesprochen uebern die Projekt-, sowie Instanz-ID.
      * In deploytem Status soll die Datenbank von der Application nur ueber einen root-Zugang und den entsprechenden Google-Treibern angesprochen werden.
      */
-    private static String googleUrl = "jdbc:google:mysql://notework-147007:noteworkinstance/noteworkdatabase";
+    private static String googleUrl = "jdbc:google:mysql://notework-147007:noteworkinstance/noteworkdatabase?user=root";
     
     /**
      *   Die URL fuer die Google Cloud-SQL Datenbank zum Ansteuern der Datenbank von einer
@@ -30,7 +30,7 @@ public class DBConnection {
      *   bereitgestellte IPv4-Adresse sowie einen eingerichteten Zugang angesprochen werden.
      */
     
-    private static String localUrl = "jdbc:mysql://104.199.71.36:3006/noteworkdatabase";
+    private static String localUrl = "jdbc:mysql://104.199.71.36:3306/noteworkdatabase";
 
     /**
      * Statische Methode zum Aufruf der <code>DBConnection.connection()</code>.
@@ -60,8 +60,8 @@ public class DBConnection {
              * Zugangsdaten fuer einen Nicht-root-Account zur Cloud SQL, der vollen Zugriff auf die DB hat.
              */
     
-             String user = "user";
-             String password = "testit";    
+             String user = "root";
+             String password = "619464532511633203";   
             
             try {
                 if (SystemProperty.environment.value() == SystemProperty.Environment.Value.Production) {
