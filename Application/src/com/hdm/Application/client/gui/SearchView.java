8d67cdb90a383bd81189f14a75380b16a9520480
andra <aw124@hdm-stuttgart.de>
2016-12-14 20:29:41 +0100

    searchView überarbeitet

--- Application/src/com/hdm/Application/client/gui/SearchView.java
+++ Application/src/com/hdm/Application/client/gui/SearchView.java
@@ -63,197 +63,211 @@ public class SearchView extends Update {
 	final RadioButton notebookButton = new RadioButton("Radiobutton-Group","Notebook");
 	
 
-	 protected void run() {
-		  
-		  // Ankï¿½ndigung, was nun geschehen wird.
-		  //Data Tables werden erzeugt.
-		  noteTable();
-		  notebookTable();
-		  userTable();
-		  
-		  this.append("");
+	protected void run() {
+		
+		// Ankï¿½ndigung, was nun geschehen wird.
+		//Data Tables werden erzeugt.
+		createNoteTable();
+		createNotebookTable();
+		userTable();
+		
+		this.append("");
 
-		  /**
-		   * Zuteilung der Widgets zum jeweiligen Panel
-		   */
+		/**
+		 * Zuteilung der Widgets zum jeweiligen Panel
+		 */
 
-		  searchPanel.add(searchButton);
-		  searchPanel.add(searchBox);
-		  searchPanel.add(searchDateBox);
-		  radiobuttonPanel.add(dueDateButton);
-		  radiobuttonPanel.add(userNameButton);
-		  radiobuttonPanel.add(noteButton);
-		  radiobuttonPanel.add(notebookButton);
-		  searchPanel.add(radiobuttonPanel);
-		  searchPanel.add(ergebnisPanel);
-		  ergebnisPanel.clear();
-		  RootPanel.get("Details").add(searchPanel);
-		  
+		searchPanel.add(searchButton);
+		searchPanel.add(searchBox);
+		searchPanel.add(searchDateBox);
+		radiobuttonPanel.add(dueDateButton);
+		radiobuttonPanel.add(userNameButton);
+		radiobuttonPanel.add(noteButton);
+		radiobuttonPanel.add(notebookButton);
+		searchPanel.add(radiobuttonPanel);
+		searchPanel.add(ergebnisPanel);
+		ergebnisPanel.clear();
+		RootPanel.get("Details").add(searchPanel);
+		
 
-		  /**
-		   * Zuweisung eines Styles fuer die jeweiligen Widgets
-		   **/
-		  searchDateBox.setVisible(false);  
-		  searchButton.setStyleName("notework-menubutton");
-		  dueDateButton.setText("Duedate");
-		  userNameButton.setText("Username");
-		  noteButton.setText("Note");
-		  notebookButton.setText("Notebook");
+		/**
+		 * Zuweisung eines Styles fuer die jeweiligen Widgets
+		 **/
+		searchDateBox.setVisible(false);		
+		searchButton.setStyleName("notework-menubutton");
+		dueDateButton.setText("Duedate");
+		userNameButton.setText("Username");
+		noteButton.setText("Note");
+		notebookButton.setText("Notebook");
 
-		  /**
-		   * Erstellung der ClickHandler
-		   **/
-		  dueDateButton.addValueChangeHandler(new ValueChangeHandler<Boolean>() {
-		   
-		   
-		   @Override
-		   public void onValueChange(ValueChangeEvent<Boolean> event) {
-		    searchBox.setVisible(false);
-		    searchDateBox.setVisible(true);
-		    //createNoteTable();
-		    
-		   }
-		  });
-		  
-		   userNameButton.addValueChangeHandler(new ValueChangeHandler<Boolean>() {
-		    
-		    
-		    @Override
-		    public void onValueChange(ValueChangeEvent<Boolean> event) {
-		    searchBox.setVisible(true);
-		    searchDateBox.setVisible(false);
-		    //userTable();
-		    
-		    }
-		   });
-		    
-		   noteButton.addValueChangeHandler(new ValueChangeHandler<Boolean>() {
-		    
-		    
-		    @Override
-		    public void onValueChange(ValueChangeEvent<Boolean> event) {
-		     searchBox.setVisible(true);
-		     searchDateBox.setVisible(false);
-		     //createNoteTable();
-		    }
-		   });
-		   notebookButton.addValueChangeHandler(new ValueChangeHandler<Boolean>() {
-		     public void onValueChange(ValueChangeEvent<Boolean> event) {
-		      searchBox.setVisible(true);
-		      searchDateBox.setVisible(false);
-		      //createNotebookTable();
-		     }
-		  
-		  });
-		  
-		  searchButton.addClickHandler(new ClickHandler() {
-		   public void onClick(ClickEvent event) {
-		    /*
-		     * Showcase instantiieren.
-		    */
-		    ergebnisPanel.clear();
-		    boolean dd = dueDateButton.getValue();
-		    boolean un = userNameButton.getValue();
-		    boolean n = noteButton.getValue();
-		    boolean nb = notebookButton.getValue();
-		    if(dd){     
-		       adminService.searchForNoteByDD(searchDateBox.getValue(), new AsyncCallback<ArrayList<Note>>() {
-		     @Override
-		     public void onFailure(Throwable caught) {
-		      Window.alert("Suche nach DueDate hat nicht funktioniert.");
-		      }
-		     public void onSuccess(ArrayList<Note> result) {
-		      if(result.size() == 0 ){
-		       Window.alert("Zu deiner Suche konnten leider keine Ergebnisse gefunden werden.");
-		        }else{
-		        ergebnisPanel.add(noteTable);        
-		        noteTable.setRowCount(result.size(), false);
-		       noteTable.setRowData(0, result);
-		       ListDataProvider<Note> dataProvider = new ListDataProvider<Note>();
-		       dataProvider.addDataDisplay(noteTable);
-		       dataProvider.setList(result);
-		       ergebnisPanel.setVisible(true);
-		      }
-		      }
-		       });
-		    }
-		    if(un){     
-		        adminService.searchForUser(searchBox.getValue(), new AsyncCallback<ArrayList<AppUser>>() {
-		      @Override
-		      public void onFailure(Throwable caught) {
-		       Window.alert("Suche nach User hat nicht funktioniert.");
-		       }
-		      public void onSuccess(ArrayList<AppUser> result) {
-		       if(result.size() == 0 ){
-		        Window.alert("Zu deiner Suche konnten leider keine Ergebnisse gefunden werden.");
-		         }else{
-		         Window.alert("Resultsize: "+result.size());
-		         ergebnisPanel.add(userTable);
-		         userTable.setRowCount(result.size(), false);
-		        userTable.setRowData(0, result);
-		        ListDataProvider<AppUser> dataProvider = new ListDataProvider<AppUser>();
-		        dataProvider.addDataDisplay(userTable);
-		        dataProvider.setList(result);
-		        ergebnisPanel.setVisible(true);
-		       }
-		       }
-		        });
-		     }
-		    if(n){     
-		        adminService.searchForNote(searchBox.getValue(), new AsyncCallback<ArrayList<Note>>() {
-		       @Override
-		       public void onFailure(Throwable caught) {
-		        Window.alert("Fehler in searchForNote Call"+caught.toString());
-		       }
+		/**
+		 * Erstellung der ClickHandler
+		 **/
+		dueDateButton.addValueChangeHandler(new ValueChangeHandler<Boolean>() {
+			
+			
+			@Override
+			public void onValueChange(ValueChangeEvent<Boolean> event) {
+				searchBox.setVisible(false);
+				searchDateBox.setVisible(true);
+				searchDateBox.showDatePicker();
+				//createNoteTable();
+				
+			}
+		});
+		
+			userNameButton.addValueChangeHandler(new ValueChangeHandler<Boolean>() {
+				
+				
+				@Override
+				public void onValueChange(ValueChangeEvent<Boolean> event) {
+				searchBox.setVisible(true);
+				searchDateBox.setVisible(false);
+				//userTable();
+				
+				}
+			});
+				
+			noteButton.addValueChangeHandler(new ValueChangeHandler<Boolean>() {
+				
+				
+				@Override
+				public void onValueChange(ValueChangeEvent<Boolean> event) {
+					searchBox.setVisible(true);
+					searchDateBox.setVisible(false);
+					//createNoteTable();
+				}
+			});
+			notebookButton.addValueChangeHandler(new ValueChangeHandler<Boolean>() {
+					public void onValueChange(ValueChangeEvent<Boolean> event) {
+						searchBox.setVisible(true);
+						searchDateBox.setVisible(false);
+						//createNotebookTable();
+					}
+		
+		});
+		
+		searchButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				ergebnisPanel.clear();
+				/*
+				 * Showcase instantiieren.
+				*/
+				boolean rbselected = false;
+				boolean dd = dueDateButton.getValue();
+				boolean un = userNameButton.getValue();
+				boolean n = noteButton.getValue();
+				boolean nb = notebookButton.getValue();
+				
+				if(dd){	
+				   rbselected = true;				   
+				   adminService.searchForNoteByDD(searchDateBox.getValue(), new AsyncCallback<ArrayList<Note>>() {
+					@Override
+					public void onFailure(Throwable caught) {
+						Window.alert("Suche nach DueDate hat nicht funktioniert.");
+						}
+					public void onSuccess(ArrayList<Note> result) {
+						if(result.size() == 0 ){
+							Window.alert("Zu deiner Suche konnten leider keine Ergebnisse gefunden werden.");
+					 	 }else{
+					 		ergebnisPanel.add(noteTable);					 		
+					 		noteTable.setRowCount(result.size(), false);
+							noteTable.setRowData(0, result);
+							ListDataProvider<Note> dataProvider = new ListDataProvider<Note>();
+							dataProvider.addDataDisplay(noteTable);
+							dataProvider.setList(result);
+							ergebnisPanel.setVisible(true);
+						}
+						}
+				   });
+				}
+				if(un){	
+					   rbselected = true;
+					   adminService.searchForUser(searchBox.getValue(), new AsyncCallback<ArrayList<AppUser>>() {
+						@Override
+						public void onFailure(Throwable caught) {
+							Window.alert("Suche nach User hat nicht funktioniert.");
+							}
+						public void onSuccess(ArrayList<AppUser> result) {
+							if(result.size() == 0 ){
+								Window.alert("Zu deiner Suche konnten leider keine Ergebnisse gefunden werden.");
+						 	 }else{
+						 		Window.alert("Resultsize: "+result.size());
+						 		ergebnisPanel.add(userTable);
+						 		userTable.setRowCount(result.size(), false);
+								userTable.setRowData(0, result);
+								ListDataProvider<AppUser> dataProvider = new ListDataProvider<AppUser>();
+								dataProvider.addDataDisplay(userTable);
+								dataProvider.setList(result);
+								ergebnisPanel.setVisible(true);
+							}
+							}
+					   });
+					}
+				if(n){			
+						rbselected = true;
+						adminService.searchForNote(searchBox.getValue(), new AsyncCallback<ArrayList<Note>>() {
+							@Override
+							public void onFailure(Throwable caught) {
+								Window.alert("Fehler in searchForNote Call"+caught.toString());
+							}
 
-		       @Override
-		       public void onSuccess(ArrayList<Note> result) {
-		        if(result.size() == 0 ){
-		         Window.alert("Zu deiner Suche konnten leider keine Ergebnisse gefunden werden.");
-		          }else{
-		          Window.alert("Resultsize: "+result.size());
-		          ergebnisPanel.add(noteTable);
-		          noteTable.setRowCount(result.size(), false);
-		         noteTable.setRowData(0, result);
-		         ListDataProvider<Note> dataProvider = new ListDataProvider<Note>();
-		         dataProvider.addDataDisplay(noteTable);
-		         dataProvider.setList(result);
-		         ergebnisPanel.setVisible(true);
-		        }
-		       }
-		      });
-		     }
-		    
-		    
-		    if(nb){     
-		        adminService.searchForNotebook(searchBox.getValue(), new AsyncCallback<ArrayList<Notebook>>() {
-		      @Override
-		      public void onFailure(Throwable caught) {
-		       Window.alert("Suche nach Note hat nicht funktioniert.");
-		       }
-		      public void onSuccess(ArrayList<Notebook> result) {
-		       if(result.size() == 0 ){
-		        Window.alert("Zu deiner Suche konnten leider keine Ergebnisse gefunden werden.");
-		         }else{
-		         Window.alert("Resultsize: "+result.size());
-		         ergebnisPanel.add(notebookTable);
-		         notebookTable.setRowCount(result.size(), false);
-		        notebookTable.setRowData(0, result);
-		        ListDataProvider<Notebook> dataProvider = new ListDataProvider<Notebook>();
-		        dataProvider.addDataDisplay(notebookTable);
-		        dataProvider.setList(result);
-		        ergebnisPanel.setVisible(true);
-		       }
-		       }
-		        });
-		     }
-		    
-//		    Update update = new ResultView();
-//		    RootPanel.get("Details").clear();
-//		    RootPanel.get("Details").add(update);
-		   }
-		  });
-		 }
+							@Override
+							public void onSuccess(ArrayList<Note> result) {
+								if(result.size() == 0 ){
+									Window.alert("Zu deiner Suche konnten leider keine Ergebnisse gefunden werden.");
+							 	 }else{
+							 		Window.alert("Resultsize: "+result.size());
+							 		ergebnisPanel.add(noteTable);
+							 		noteTable.setRowCount(result.size(), false);
+									noteTable.setRowData(0, result);
+									ListDataProvider<Note> dataProvider = new ListDataProvider<Note>();
+									dataProvider.addDataDisplay(noteTable);
+									dataProvider.setList(result);
+									ergebnisPanel.setVisible(true);
+								}
+							}
+						});
+					}
+				
+				
+				if(nb){			
+					   rbselected = true;
+					   adminService.searchForNotebook(searchBox.getValue(), new AsyncCallback<ArrayList<Notebook>>() {
+						@Override
+						public void onFailure(Throwable caught) {
+							Window.alert("Suche nach Note hat nicht funktioniert.");
+							}
+						public void onSuccess(ArrayList<Notebook> result) {
+							if(result.size() == 0 ){
+								Window.alert("Zu deiner Suche konnten leider keine Ergebnisse gefunden werden.");
+						 	 }else{
+						 		Window.alert("Resultsize: "+result.size());
+						 		ergebnisPanel.add(notebookTable);
+						 		notebookTable.setRowCount(result.size(), false);
+								notebookTable.setRowData(0, result);
+								ListDataProvider<Notebook> dataProvider = new ListDataProvider<Notebook>();
+								dataProvider.addDataDisplay(notebookTable);
+								dataProvider.setList(result);
+								ergebnisPanel.setVisible(true);
+							}
+						 }
+					   });
+					}
+				
+				if (!rbselected) {
+					Window.alert("Bitte erst ein Radiobutton ausgewählen!");
+					searchBox.setFocus(true);
+					searchBox.setText("");
+					searchDateBox.setValue(null);
+				}
+				
+//				Update update = new ResultView();
+//				RootPanel.get("Details").clear();
+//				RootPanel.get("Details").add(update);
+			}
+		});
+	}
 
 	private void userTable(){
 		   TextColumn<AppUser> nameCol = new TextColumn<AppUser>() {
@@ -278,7 +292,7 @@ public class SearchView extends Update {
 		 userTable.addColumn(googleIDCol, "E-Mail");
 	}
 	
-	private void notebookTable(){
+	protected void createNotebookTable(){
 			
 		TextColumn<Notebook> titleCol = new TextColumn<Notebook>() {
 
@@ -326,7 +340,7 @@ public class SearchView extends Update {
 
 	
 	
-   protected void noteTable(){
+   protected void createNoteTable(){
 
 
 		TextColumn<Note> titleCol = new TextColumn<Note>() {
